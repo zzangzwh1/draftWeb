7e8b50b3bd78b8a738f9c3fad7aec83487bd11c0
Wonhyuk Cho <zzangzwh@hotmail.com>
2023-05-13 19:48:55 -0600

    simple web

---
+++ WH_draftWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WH_draftWeb.Models;
+using WH_draftWeb.Services;
+
+namespace WH_draftWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService _jsonFileProductService;
+        public IEnumerable<Product> products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger,JsonFileProductService productService)
+        {
+            _logger = logger;
+            _jsonFileProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            products = _jsonFileProductService.GetProducts();
+        }
+    }
+}
---
+++ WH_draftWeb/Services/JsonFileProductService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using System.Text.Json;
+using WH_draftWeb.Models;
+using System.IO;
+
+namespace WH_draftWeb.Services
+{
+    public class JsonFileProductService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var fileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(fileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
